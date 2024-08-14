4748ba4cfde9b9a314cadbc33e408a71290a9e46
Valentin <zhe-valentin@mail.ru>
2024-08-14 21:51:53 +0700

    hw-4 cycles 1-2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача №1");
+        // сколько месяцев потребуется, чтобы накопить 2 459 000 рублей
+        int totalSum = 0; // сумма итогового накопления
+        int i = 0; // месяцы
+        int contribution = 15_000; // ежемесячный вклад
+        while (totalSum < 2_459_000) {
+            i++;
+            totalSum = totalSum + contribution;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " +
+                    totalSum + " рублей");
+        }
+
+        System.out.println("Задача №2");
+        // числа от 1 до 10 одной строкой через пробел
+        int a = 0;
+        while (a < 10) {
+            a++;
+            System.out.print(a + " ");
+
+        }
+        System.out.println();
+        for (int b = 10; b > 0; b--) {
+            System.out.print(b + " ");
+
+        }
+        System.out.println();
+
+        System.out.println("Задача №3");
+        //
     }
 }
